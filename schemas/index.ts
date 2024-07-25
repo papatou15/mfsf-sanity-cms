12bce9a9d361c7741584e0e9e9231b8ff20a0096
Antoine Ridard <papatou15@hotmail.com>
2024-07-25 18:50:43 -0400

    form 6-12 added

--- schemas/index.ts
+++ schemas/index.ts
@@ -1,5 +1,6 @@
 import { inscriptionType } from "./inscriptions"
 import { eventType } from "./event"
 import { activityType } from "./activity"
+import { form612 } from "./form612"
 
-export const schemaTypes = [inscriptionType, eventType, activityType]
+export const schemaTypes = [inscriptionType, eventType, activityType, form612]
